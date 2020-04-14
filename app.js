5df524abae56e1cc914aaac2658822b1601f0512
Giannis Pitsakis <gp@intelligentmedia.gr>
2020-04-14 16:24:40 +0300

    testing4

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req,res)=>{
     res.send("this is a test part2")
 })
 
-const port=process.env.PORT || 6000
+const port=process.env.PORT || 3001
 app.listen(port,()=>{
-    console.log("Server Has started on 3000");
+    console.log("Server Has started on 3001");
 });
